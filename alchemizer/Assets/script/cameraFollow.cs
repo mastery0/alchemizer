676111ced646b61f18654362d17d694601b0540d
mastery0 <giulio.dibartolo23@gmail.com>
2026-06-12 19:52:17 +0200

    playerMovement

---
+++ alchemizer/Assets/script/cameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed = 5f;
+    public Vector3 offsetX =new Vector3(-2, 0, 0);
+    private Vector3 targetPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos+offsetX, followSpeed * Time.deltaTime);
+    }
+}
---
+++ alchemizer/Assets/script/player.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player : MonoBehaviour
+{
+    public static player instance;
+    public Rigidbody2D prb;
+    public LayerMask ground;
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float fastFallForce = 3f;
+    public float dashForce = 5f;
+    public float dashTime = 0.2f;
+    public float dashCooldown = 1f;
+    [Header("Combat")]
+    public float hp;
+    public float maxHp = 100f;
+    public float attackDamage = 10f;
+    public float attackRange = 1f;
+    public float attackCooldown = 0.5f;
+
+
+    private float moveX;
+    private bool isDashing;
+    private bool grounded;
+    private bool canDash = true;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isDashing) prb.linearVelocity = new Vector2(moveX * moveSpeed, prb.linearVelocityY);
+        grounded = Physics2D.OverlapCircle(transform.position, 0.9f, ground);
+
+    }
+    // Input System
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveX = context.ReadValue<Vector2>().x;
+        if (context.ReadValue<Vector2>().y > 0 && grounded)
+        {
+            jump();
+        }
+        if (context.ReadValue<Vector2>().y < 0)
+        {
+            fastFall();
+        }
+    }
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.started && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        Debug.Log("Dashing");
+        isDashing = true;
+        canDash = false;
+        prb.linearVelocity=new Vector2(moveX * dashForce, 0);
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+    public void jump()
+    {
+        prb.linearVelocity=new Vector2( prb.linearVelocityX, jumpForce);
+    }
+    public void fastFall()
+    {
+        prb.linearVelocity = new Vector2(prb.linearVelocityX, -fastFallForce);
+    }
+    
+}
