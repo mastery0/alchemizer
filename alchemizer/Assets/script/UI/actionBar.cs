f27c5894cbaf6900f939dc9f3f1f3f50b37bd561
mastery0 <giulio.dibartolo23@gmail.com>
2026-07-10 16:47:11 +0200

    questSystem3

--- alchemizer/Assets/script/UI/actionBar.cs
+++ alchemizer/Assets/script/UI/actionBar.cs
@@ -5,34 +5,47 @@ public class actionBar : MonoBehaviour
     public GameObject skillTree;
     public GameObject inventory;
     public GameObject pauseMenu;
+    public GameObject questMenu;
     private void OnEnable()
     {
         skillTree.SetActive(false);
         inventory.SetActive(true);
         pauseMenu.SetActive(false);
+        questMenu.SetActive(false);
     }
     private void OnDisable()
     {
         skillTree.SetActive(false);
         inventory.SetActive(false);
         pauseMenu.SetActive(false);
+        questMenu.SetActive(false);
     }
     public void OpenSkillTree()
     {
         skillTree.SetActive(true);
         inventory.SetActive(false);
         pauseMenu.SetActive(false);
+        questMenu.SetActive(false);
     }
     public void OpenInventory()
     {
         skillTree.SetActive(false);
         inventory.SetActive(true);
         pauseMenu.SetActive(false);
+        questMenu.SetActive(false);
     }
     public void OpenPauseMenu()
     {
         skillTree.SetActive(false);
         inventory.SetActive(false);
         pauseMenu.SetActive(true);
+        questMenu.SetActive(false);
+    }
+    public void OpenQuestMenu()
+    {
+        skillTree.SetActive(false);
+        inventory.SetActive(false);
+        pauseMenu.SetActive(false);
+        questMenu.SetActive(true);
     }
 }
